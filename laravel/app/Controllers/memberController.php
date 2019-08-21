880514f999c62a71ab2f15cdcd41ce06c77ed522
Kazi Tanveer <43889065+Tanveer16-31347-1@users.noreply.github.com>
2019-08-21 15:47:35 +0600

    Add files via upload

--- laravel/app/Controllers/memberController.php
+++ laravel/app/Controllers/memberController.php
@@ -39,6 +39,37 @@ class memberController extends Controller
 
 
 
+
+        public function updateMember($c_id,Request $req){
+        
+    
+            //updates
+
+            $newUserRow = DB::table('t_carlist')->where('c_id', $c_id)->get();
+
+                        
+                        DB::table('t_carlist')->where('c_id', $c_id)->delete(); //Delete New Users
+
+            DB::table('t_rented')->insert([
+                ['r_name' => $newUserRow[0]->c_name,  
+                'r_brand' => $newUserRow[0]->c_brand ,
+                'r_price' => $newUserRow[0]->c_price ,
+                'r_type' => $newUserRow[0]->c_type ,
+                             
+            ]
+                
+            ]);      
+            //insert to user ends                
+
+                    $req->session()->flash('msg', "✔ New User Added To Your Portal");
+                            return redirect()->route('member.carlist');
+                    
+
+    }
+
+
+
+
 }
 
 
